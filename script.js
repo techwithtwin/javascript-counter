d56292f0a6c95fed325e0f2366e6201a8414d37f
TechWithTwin <realwebtechguru@gmail.com>
2025-07-20 23:49:59 +0300

    adding increment and reset functionality

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const decrementBtn = document.getElementById("decrement");
 const resetBtn = document.getElementById("reset");
 
 decrementBtn.addEventListener("click", decrement);
+incrementBtn.addEventListener("click", increment);
+resetBtn.addEventListener("click", reset);
 
 function decrement() {
   // do something when the button is clicked.
@@ -12,4 +14,17 @@ function decrement() {
 
   // now let's set the new value to the counter element;
   counter.innerText = newCounterValue;
+}
+
+function increment() {
+  // do something when the button is clicked.
+  const counterValue = counter.innerText;
+  const newCounterValue = Number(counterValue) + 1;
+
+  // now let's set the new value to the counter element;
+  counter.innerText = newCounterValue;
+}
+
+function reset() {
+  counter.innerText = 0;
 }
